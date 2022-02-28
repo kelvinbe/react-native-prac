0b71d7d7f36cd5891c20a740e9fe562d613f2876
kelvin beno <kelvinbeno526@gmail.com>
2022-02-28 10:04:20 +0300

    some updates

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ScrollView, FlatList, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
+  // const [name, setname] = useState("kelvin");
+  const [people, setPeople] = useState([
+    { name: "Mario", key: 7 },
+    { name: "moris", key: 6 },
+    { name: "James", key: 8 },
+    { name: "Trevor", key: 9 },
+    { name: "Havier", key: 1 },
+    { name: "Coby", key: 2 },
+    { name: "Janusz", key: 3 },
+    { name: "LumpaLumpa", key: 4 },
+  ]);
+
+  const pressHandler = (key) => {
+      // console.log(key)
+      setPeople((prevPeople) => {
+        return prevPeople.filter(person => person.key != key)
+      })
+
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <FlatList 
+
+      keyExtractor={(item) => item.key}
+      data={people}
+      renderItem={({item}) => (
+        <TouchableOpacity style={styles.item} onPress={() => pressHandler(item.key)}>
+        <Text style={styles.item}>
+          {item.name}
+        </Text>
+        </TouchableOpacity>
+      )}
+       />
+
+
+
+      {/* <ScrollView>
+
+      {
+        people.map((item) => {
+          return (
+              <View>
+                <Text style={styles.item} key={item.key}>
+                  {item.name}
+                </Text>
+              </View>
+          )
+        })
+      }
+      </ScrollView> */}
     </View>
   );
 }
@@ -13,8 +61,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  item: {
+    marginTop: 30,
+    padding: 20,
+    backgroundColor: "pink",
+    fontSize: 24,
   },
 });
